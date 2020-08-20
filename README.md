fad5a541bccd43f9efd88999eeacbf9787da9a9e
Le Bookmaker <69488905+LeBookmaker@users.noreply.github.com>
2020-08-20 13:35:16 -0400

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
 # book_weather
 Change the weather with chat command
+
+## Chat command
+```/blizzard ```	
+```/clouds ```
+```/drizzle ```	
+```/fog ```	
+```/groundblizzard ```	
+```/hail ```	
+```/highpressure ```	
+```/hurricane ```	
+```/misty ```	
+```/overcast ```	
+```/overcastdark ```	
+```/rain	 ```
+```/sandstorm ```	
+```/shower	 ```
+```/sleet	 ```
+```/snow	 ```
+```/snowclearing	 ```
+```/snowlight	 ```
+```/sunny	 ```
+```/thunder	 ```
+```/thunderstorm	 ```
+```/whiteout ```	
+
+## How to install
+* Copy and paste ```book_weather``` folder to ```resources/book_weather```
+* Add ```ensure book_weather``` to your ```server.cfg``` file
+* Enjoy!
+
---
+++ client.lua
@@ -0,0 +1,117 @@
+--All register command change the weather :)
+
+RegisterCommand('blizzard',function()
+	Citizen.InvokeNative(0x59174F1AFE095B5A, 0x27EA2814, true, true, true, true, true)
+	TriggerEvent("vorp:TipBottom", 'Change to blizzard', 10000)
+end)
+
+RegisterCommand('clouds',function()
+	Citizen.InvokeNative(0x59174F1AFE095B5A, 0x30FDAF5C, true, true, true, true, true)
+	TriggerEvent("vorp:TipBottom", 'Change to clouds', 10000)
+end)
+
+RegisterCommand('drizzle',function()
+	Citizen.InvokeNative(0x59174F1AFE095B5A, 0x995C7F44, true, true, true, true, true)
+	TriggerEvent("vorp:TipBottom", 'Change to drizzle', 10000)
+end)
+
+RegisterCommand('fog',function()
+	Citizen.InvokeNative(0x59174F1AFE095B5A, 0xD61BDE01, true, true, true, true, true)
+	TriggerEvent("vorp:TipBottom", 'Change to fog', 10000)
+end)
+
+RegisterCommand('groundblizzard',function()
+	Citizen.InvokeNative(0x59174F1AFE095B5A, 0x7F622122, true, true, true, true, true)
+	TriggerEvent("vorp:TipBottom", 'Change to groundblizzard', 10000)
+end)
+
+RegisterCommand('hail',function()
+	Citizen.InvokeNative(0x59174F1AFE095B5A, 0x75A9E268, true, true, true, true, true)
+	TriggerEvent("vorp:TipBottom", 'Change to hail', 10000)
+end)
+
+RegisterCommand('highpressure',function()
+	Citizen.InvokeNative(0x59174F1AFE095B5A, 0xF5A87B65, true, true, true, true, true)
+	TriggerEvent("vorp:TipBottom", 'Change to highpressure', 10000)
+end)
+
+RegisterCommand('hurricane',function()
+	Citizen.InvokeNative(0x59174F1AFE095B5A, 0x320D0951, true, true, true, true, true)
+	TriggerEvent("vorp:TipBottom", 'Change to hurricane', 10000)
+end)
+
+RegisterCommand('misty',function()
+	Citizen.InvokeNative(0x59174F1AFE095B5A, 0x5974E8E5, true, true, true, true, true)
+	TriggerEvent("vorp:TipBottom", 'Change to misty', 10000)
+end)
+
+RegisterCommand('overcast',function()
+	Citizen.InvokeNative(0x59174F1AFE095B5A, 0xBB898D2D, true, true, true, true, true)
+	TriggerEvent("vorp:TipBottom", 'Change to overcast', 10000)
+end)
+
+RegisterCommand('overcastdark',function()
+	Citizen.InvokeNative(0x59174F1AFE095B5A, 0x19D4F1D9, true, true, true, true, true)
+	TriggerEvent("vorp:TipBottom", 'Change to overcastdark', 10000)
+end)
+
+RegisterCommand('rain',function()
+	Citizen.InvokeNative(0x59174F1AFE095B5A, 0x54A69840, true, true, true, true, true)
+	TriggerEvent("vorp:TipBottom", 'Change to rain', 10000)
+end)
+
+RegisterCommand('sandstorm',function()
+	Citizen.InvokeNative(0x59174F1AFE095B5A, 0xB17F6111, true, true, true, true, true)
+	TriggerEvent("vorp:TipBottom", 'Change to sandstorm', 10000)
+end)
+
+RegisterCommand('shower',function()
+	Citizen.InvokeNative(0x59174F1AFE095B5A, 0xE72679D5, true, true, true, true, true)
+	TriggerEvent("vorp:TipBottom", 'Change to shower', 10000)
+end)
+
+RegisterCommand('sleet',function()
+	Citizen.InvokeNative(0x59174F1AFE095B5A, 0xCA71D7C, true, true, true, true, true)
+	TriggerEvent("vorp:TipBottom", 'Change to sleet', 10000)
+end)
+
+RegisterCommand('snow',function()
+	Citizen.InvokeNative(0x59174F1AFE095B5A, 0xEFB6EFF6, true, true, true, true, true)
+	TriggerEvent("vorp:TipBottom", 'Change to snow', 10000)
+end)
+
+RegisterCommand('snowclearing',function()
+	Citizen.InvokeNative(0x59174F1AFE095B5A, 0x641DFC11, true, true, true, true, true)
+	TriggerEvent("vorp:TipBottom", 'Change to snowclearing', 10000)
+end)
+
+RegisterCommand('snowlight',function()
+	Citizen.InvokeNative(0x59174F1AFE095B5A, 0x23FB812B, true, true, true, true, true)
+	TriggerEvent("vorp:TipBottom", 'Change to snowlight', 10000)
+end)
+
+RegisterCommand('sunny',function()
+	Citizen.InvokeNative(0x59174F1AFE095B5A, 0x614A1F91, true, true, true, true, true)
+	TriggerEvent("vorp:TipBottom", 'Change to sunny', 10000)
+end)
+
+RegisterCommand('thunder',function()
+	Citizen.InvokeNative(0x59174F1AFE095B5A, 0xB677829F, true, true, true, true, true)
+	TriggerEvent("vorp:TipBottom", 'Change to thunder', 10000)
+end)
+
+RegisterCommand('thunderstorm',function()
+	Citizen.InvokeNative(0x59174F1AFE095B5A, 0x7C1C4A13, true, true, true, true, true)
+	TriggerEvent("vorp:TipBottom", 'Change to thunderstorm', 10000)
+end)
+
+RegisterCommand('whiteout',function()
+	Citizen.InvokeNative(0x59174F1AFE095B5A, 0x2B402288, true, true, true, true, true)
+	TriggerEvent("vorp:TipBottom", 'Change to whiteout', 10000)
+end)
+
+
+
+
+
+
